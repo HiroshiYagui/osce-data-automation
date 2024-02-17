176aad7bd0816f41349582f127cbe4449b9c0077
Hiroshi Yagui <hiroshiyagui@gmail.com>
2024-02-16 21:44:07 -0500

    added BSOUP script

---
+++ src/bsoup_script.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += 'HIGH:!DH:!aNULL'
+try:
+    requests.packages.urllib3.contrib.pyopenssl.DEFAULT_SSL_CIPHER_LIST += 'HIGH:!DH:!aNULL'
+except AttributeError:
+    # no pyopenssl support used / needed / available
+    pass
+
+url='https://easprep.osce.gob.pe/portaltribunal-uiwd-pub/ControllerServlet'
+cookie={'JSESSIONID':'cGmELYffoEGrRIFMC02nwfr6.31343ada-59ff-3a71-b141-1abc3a553f94'}
+page=requests.get(url,cookies=cookie,verify=False)
+
+soup=BeautifulSoup(page.text,'html')
+
+print(soup)
